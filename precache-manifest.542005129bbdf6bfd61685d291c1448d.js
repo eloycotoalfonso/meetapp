993cc9a854ac1440714daff15324f1a3cd931351
eloycotoalfonso <eloycoal@gmail.com>
2023-01-20 12:08:48 +0100

    Updates

--- precache-manifest.542005129bbdf6bfd61685d291c1448d.js
+++ precache-manifest.542005129bbdf6bfd61685d291c1448d.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "ddd3e09a19093f507569",
+    "revision": "e255d54b788c8ff41e18",
     "url": "/meetapp/static/css/main.d7dc7d52.chunk.css"
   },
   {
-    "revision": "ddd3e09a19093f507569",
-    "url": "/meetapp/static/js/main.8de4636b.chunk.js"
+    "revision": "e255d54b788c8ff41e18",
+    "url": "/meetapp/static/js/main.1a383270.chunk.js"
   },
   {
     "revision": "05097e6efdc1fe89d889",
     "url": "/meetapp/static/js/runtime~main.d851fbd8.js"
   },
   {
-    "revision": "448e810d436034a49372",
-    "url": "/meetapp/static/js/2.14266a25.chunk.js"
+    "revision": "37df3edf522d466abc8b",
+    "url": "/meetapp/static/js/2.9ec4aed4.chunk.js"
   },
   {
     "revision": "6cb4e366ddc73ab361e9",
     "url": "/meetapp/static/js/3.e1886a8b.chunk.js"
   },
   {
-    "revision": "80b90c565b4db3b3716c25d81aef5d49",
+    "revision": "f91444cccc8ddd4843345a5584572378",
     "url": "/meetapp/index.html"
   }
 ];
