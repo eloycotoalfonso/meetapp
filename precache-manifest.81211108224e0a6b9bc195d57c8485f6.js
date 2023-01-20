b1f23a088ba6f1c8aafa24751d189aabca87af85
eloycotoalfonso <eloycoal@gmail.com>
2023-01-20 11:03:43 +0100

    Updates

---
+++ precache-manifest.81211108224e0a6b9bc195d57c8485f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "92dd57e952e329195732",
+    "url": "/meetapp/static/css/main.d7dc7d52.chunk.css"
+  },
+  {
+    "revision": "92dd57e952e329195732",
+    "url": "/meetapp/static/js/main.4b05ed28.chunk.js"
+  },
+  {
+    "revision": "05097e6efdc1fe89d889",
+    "url": "/meetapp/static/js/runtime~main.d851fbd8.js"
+  },
+  {
+    "revision": "448e810d436034a49372",
+    "url": "/meetapp/static/js/2.14266a25.chunk.js"
+  },
+  {
+    "revision": "6cb4e366ddc73ab361e9",
+    "url": "/meetapp/static/js/3.e1886a8b.chunk.js"
+  },
+  {
+    "revision": "cd51dc22fcb42277288c8553747c81b3",
+    "url": "/meetapp/index.html"
+  }
+];
