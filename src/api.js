5f6b9a2999350e52b3aec9767e93a8650d4a0efc
eloycotoalfonso <eloycoal@gmail.com>
2023-01-23 18:30:51 +0100

    debugged api js

--- src/api.js
+++ src/api.js
@@ -65,7 +65,7 @@ export const getEvents = async () => {
     if(!navigator.onLine) {
         const data = localStorage.getItem('lastEvents');
         NProgress.done();
-        return data ? JSON.parse(data) : [];
+        return data ? JSON.parse(data).events : [];;
     }
 
 
