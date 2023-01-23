72eae45894fec75488790f2c82489c171dab278d
eloycotoalfonso <eloycoal@gmail.com>
2023-01-23 19:59:10 +0100

    added verification docs and processes

--- src/api.js
+++ src/api.js
@@ -17,7 +17,7 @@ export const extractLocations = (events) => {
     return locations;
 };
 
-const checkToken = async (accessToken) => {
+export const checkToken = async (accessToken) => {
     const result = await fetch(
         `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
     )
@@ -65,7 +65,7 @@ export const getEvents = async () => {
     if(!navigator.onLine) {
         const data = localStorage.getItem('lastEvents');
         NProgress.done();
-        return data ? JSON.parse(data).events : [];;
+        return data ? JSON.parse(data).events : [];
     }
 
 
